0aef24d1e92d4efef57dc3e948f0f6c59b98bfa8
Neo Candidate <dhavalp.neo@gmail.com>
2019-03-31 23:39:25 +0530

    -- UI Update -- Testcases added

--- app/src/main/java/com/assignment/nytimes/ui/newsfeed/list/view/NewsFeedActivity.java
+++ app/src/main/java/com/assignment/nytimes/ui/newsfeed/list/view/NewsFeedActivity.java
@@ -69,6 +69,5 @@ public class NewsFeedActivity extends BaseActivity<ActivityNewsFeedBinding, News
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return super.onSupportNavigateUp();
-
     }
 }
